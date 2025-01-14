922a503755cbc3cfd937b93db8f64e1f5b784150
Kevin Demetro <albinodemetrok@gmail.com>
2025-01-14 19:59:15 -0300

    utilizando react fragment para retornar mais de um elemento

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -76,11 +76,17 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cusine. 6 creative dishes to choose from. All from
+            our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on out menu. Please come back later :)</p>
       )}
