33076cd7faa384834c16b21b9735f897e9ff2b30
Kevin Demetro <albinodemetrok@gmail.com>
2025-01-16 19:45:27 -0300

    resolvendo desafio date-counter

---
+++ date-counter/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  function handlerStepMinus() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handlerStepPlus() {
+    setStep((s) => s + 1);
+  }
+
+  function handlerCountMinus() {
+    setCount((c) => c - step);
+  }
+
+  function handlerCountPlus() {
+    setCount((c) => c + step);
+  }
+
+  return (
+    <>
+      <div>
+        <button onClick={handlerStepMinus}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={handlerStepPlus}>+</button>
+      </div>
+      <div>
+        <button onClick={handlerCountMinus}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handlerCountPlus}>+</button>
+      </div>
+      <p>
+        {count} days from today is {new Date().getDate() + count}
+      </p>
+    </>
+  );
+}
