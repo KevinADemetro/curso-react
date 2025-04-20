df269543b1660f61edc4b3de8c2de23cd99f1d56
Kevin Demetro <albinodemetrok@gmail.com>
2025-04-20 17:30:02 -0300

    adicinando funcionalidades de remover, filtrar e mudar o estado de ativo

--- browser-extension-manager/src/App.js
+++ browser-extension-manager/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import extensionsData from "./data.json";
 
 function App() {
@@ -9,49 +10,106 @@ function App() {
 }
 
 function ExtensionsSection() {
+  const [allExtensions, setAllExtensions] = useState(extensionsData);
+  const [extensionsList, setExtensionList] = useState(extensionsData);
+  const [selectedFilter, setSelectedFilter] = useState("Todos");
+
+  function handleRemoveExetension(id) {
+    setExtensionList((extensions) =>
+      extensions.filter((extension) => extension.id !== id)
+    );
+  }
+
+  function handleFilter(filter) {
+    setSelectedFilter(filter);
+
+    if (filter === "Todos") {
+      setExtensionList(extensionsData);
+    } else if (filter === "Ativo") {
+      setExtensionList((extensions) =>
+        extensions.filter((extension) => extension.isActive)
+      );
+    } else if (filter === "Inativo") {
+      setExtensionList((extensions) =>
+        extensions.filter((extension) => !extension.isActive)
+      );
+    }
+  }
+
+  function handleChangeActive(id) {
+    setExtensionList((extensions) =>
+      extensions.map((extension) =>
+        extension.id === id
+          ? { ...extension, isActive: !extension.isActive }
+          : extension
+      )
+    );
+  }
   return (
     <section>
       <div className="header-section">
         <h2>Lista de Extensões</h2>
-        <ExtensionsFilters />
+        <ExtensionsFilters
+          onFilter={handleFilter}
+          selectedFilter={selectedFilter}
+        />
       </div>
-      <ExtensionsGrid />
+      <ExtensionsGrid
+        extensionsList={extensionsList}
+        onRemoveExtension={handleRemoveExetension}
+        onChangeActive={handleChangeActive}
+      />
     </section>
   );
 }
 
-function ExtensionsFilters() {
+function ExtensionsFilters({ onFilter, selectedFilter }) {
   return (
     <ul className="filters">
-      <Filter>Todos</Filter>
-      <Filter>Ativo</Filter>
-      <Filter>Inativo</Filter>
+      <Filter onClick={onFilter} selectedFilter={selectedFilter}>
+        Todos
+      </Filter>
+      <Filter onClick={onFilter} selectedFilter={selectedFilter}>
+        Ativo
+      </Filter>
+      <Filter onClick={onFilter} selectedFilter={selectedFilter}>
+        Inativo
+      </Filter>
     </ul>
   );
 }
 
-function Filter({ children }) {
+function Filter({ children, onClick, selectedFilter }) {
+  const isSelected = selectedFilter === children;
   return (
     <li>
-      <button className="button">{children}</button>
+      <button
+        className={`button ${isSelected ? "active" : ""}`}
+        onClick={() => onClick(children)}
+      >
+        {children}
+      </button>
     </li>
   );
 }
 
-function ExtensionsGrid() {
-  const extensionsList = extensionsData;
+function ExtensionsGrid({ extensionsList, onRemoveExtension, onChangeActive }) {
   return (
     <ul className="extensions-list">
       {extensionsList.map((extension) => (
-        <li>
-          <ExtensionCard extension={extension} />
+        <li key={extension.id}>
+          <ExtensionCard
+            extension={extension}
+            onClick={onRemoveExtension}
+            onChangeActive={onChangeActive}
+          />
         </li>
       ))}
     </ul>
   );
 }
 
-function ExtensionCard({ extension }) {
+function ExtensionCard({ extension, onClick, onChangeActive }) {
   return (
     <div className="card">
       <div className="card-header">
@@ -62,17 +120,23 @@ function ExtensionCard({ extension }) {
         </div>
       </div>
       <div className="card-buttons">
-        <button className="button">Remover</button>
-        <Switch isChecked={extension.isActive} />
+        <button className="button" onClick={() => onClick(extension.id)}>
+          Remover
+        </button>
+        <Switch extension={extension} onChangeActive={onChangeActive} />
       </div>
     </div>
   );
 }
 
-function Switch({ isChecked }) {
+function Switch({ extension, onChangeActive }) {
   return (
     <label className="switch">
-      <input type="checkbox" checked={isChecked} />
+      <input
+        type="checkbox"
+        checked={extension.isActive}
+        onChange={() => onChangeActive(extension.id)}
+      />
       <span className="slider"></span>
     </label>
   );
